0f4794b8961538331ebd985fe8d598183e162b84
Deepak Saini <ds954642@gmail.com>
2025-07-04 02:51:27 +0530

    Deploy the helpdesk

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
 import NewTicket from "./pages/NewTicket";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import MyTickets from "./pages/MyTickets";
 import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Router>i */}
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
@@ -22,7 +21,6 @@ function App() {
         <Route path="/mytickets" element={<MyTickets />} />
         <Route path="/profile" element={<UserProfile />} />
       </Routes>
-      {/* </Router> */}
     </BrowserRouter>
   );
 }

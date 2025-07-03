0a55ac6dc05f5af1f2c18812734de8c309a3ca84
Deepak Saini <ds954642@gmail.com>
2025-07-04 02:45:06 +0530

    build the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/Signup";
